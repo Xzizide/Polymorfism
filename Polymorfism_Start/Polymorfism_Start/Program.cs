bac3c7faed182ba5df88cdfd3d0e8e59648ce5d4
Xzizide <59053660+Xzizide@users.noreply.github.com>
2020-10-02 10:27:50 +0200

    Add files via upload

---
+++ Polymorfism_Start/Polymorfism_Start/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Polymorfism_Start
+{
+    class Aliens //basklassen
+    {
+        virtual public void alienSound()
+        {
+            Console.WriteLine("Alien say WOOOOOOO");
+        }
+    }
+
+    class Marsian : Aliens //klass som ärver från basklassen
+    {
+        override public void alienSound()
+        {
+            Console.WriteLine("Marsian say Skrrrt");
+        }
+    }
+    class Earthean : Aliens //klass som ärver från basklassen
+    {
+        override public void alienSound()
+        {
+            Console.WriteLine("Earhtean say Hello");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aliens[] aliens = {new Earthean(), new Marsian(), new Aliens() }; //array med aliens
+
+            foreach(var i in aliens)
+            {
+                i.alienSound(); //skriver ut olika alienSounds på grund av virtual och override funktioner
+            }
+        }
+    }
+}
